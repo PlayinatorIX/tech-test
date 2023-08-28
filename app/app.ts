af9a3590e4625d90878dd13e61b9f29a9d2893e9
m lala <bionixs88@gmail.com>
2023-08-28 11:53:12 +0100

    my project

--- app/app.ts
+++ app/app.ts
@@ -2,17 +2,25 @@ import cors from 'cors';
 import express from 'express';
 import router from './routes/zendesk';
 
-const app: express.Application = express();
+// const express = require('express')
+const app = express();
+const port = 8080;
 
 const options: cors.CorsOptions = {
-  origin: '*'
+  origin: '*',
 };
 
 app.use(express.json());
 app.use(cors(options));
-app.use('/zendesk', router)
 
-app.listen('8080', function() {
-  console.log('listening on http://localhost:8080');
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
+
+app.use('/zendesk', router);
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
+
 
--- app/routes/zendesk.ts
+++ app/routes/zendesk.ts
@@ -1,12 +1,107 @@
 import express from "express";
+import axios from "axios";
+import dns from "dns";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/pages', async (req, res) => {
+// Check if the company has a Zendesk login page (CMD: curl "http://localhost:8080/zendesk/check-login/[INSERT_NAME]"  )
+router.get("/check-login/:name", async (req, res) => {
+  const name = req.params.name;
+  const loginUrl = `https://${name}.zendesk.com`;
+  try {
+    const response = await axios.get(loginUrl);
+    if (response.status === 200) {
+      res.status(200).json({ hasLogin: true });
+    } else {
+      res.status(404).json({ hasLogin: false });
+    }
+  } catch (error) {
+    res.status(404).json({ hasLogin: false });
+  }
+});
 
-  const companyUrl = req.body.companyUrl;
-  
-  res.send({ loginPage: '', supportPage: '' });
-})
 
-export default router; 
+
+// Check if the company has a support page pointing to a Zendesk host ((CMD: curl "http://localhost:8080/zendesk/check-support/[INSERT_NAME]"  ))
+router.get("/check-support/:name", async (req, res) => {
+  const name = req.params.name;
+  const supportCnames = ["support", "help"]; // Define possible support subdomains
+
+  const results = await Promise.all(
+    supportCnames.map((cname) => {
+      return new Promise((resolve) => {
+        dns.resolveCname(`${cname}.${name}.com`, (err, addresses) => {
+          if (!err) {
+            const hasZendeskCname = addresses.some((address) =>
+              address.endsWith(".zendesk.com")
+            );
+            resolve(hasZendeskCname);
+          } else {
+            resolve(false);
+          }
+        });
+      });
+    })
+  );
+  if (results.includes(true)) {
+    res.status(200).json({ isUsingZendesk: true });
+  } else {
+    res.status(404).json({ isUsingZendesk: false });
+  }
+});
+
+
+
+router.post("/check-domains", async (req, res) => { //curl -X POST -H "Content-Type: application/json" -d "{\"domains\": [\"FIRST_DOMAIN\", \"SECOND_DOMAIN\", \"Nth_DOMAIN\"]}" http://localhost:8080/zendesk/check-domains
+  const companyDomains: string[] = req.body.domains;
+  // console.log("Received request with domains:", companyDomains);
+  const results = [];
+  for (const domain of companyDomains) {
+    console.log("Processing domain:", domain);
+    const loginUrl = `https://${domain}.zendesk.com`;
+    let zendeskLoginUrl = null;
+    let zendeskSupportUrl = null;
+    try {
+      const loginResponse = await axios.get(loginUrl);
+      if (loginResponse.status === 200) {
+        zendeskLoginUrl = loginUrl;
+      }
+    } catch (error) {
+      console.error("Error checking login page for", domain);
+    }
+    const supportCnames = ["support", "help"];
+    const supportResults = await Promise.all(
+      supportCnames.map((cname) => {
+        return new Promise((resolve) => {
+          dns.resolveCname(`${cname}.${domain}.com`, (err, addresses) => {
+            if (!err) {
+              const hasZendeskCname = addresses.some((address) =>
+                address.endsWith(".zendesk.com")
+              );
+              resolve(hasZendeskCname);
+            } else {
+              resolve(false);
+            }
+          });
+        });
+      })
+    );
+
+    if (supportResults.includes(true)) {
+      zendeskSupportUrl = `https://${supportResults.find(Boolean)}.${domain}.zendesk.com`;
+    }
+
+    results.push({
+      domain,
+      zendeskLoginUrl,
+      zendeskSupportUrl,
+    });
+  }
+  // console.log("Sending response:", results);
+  const formattedResponse = JSON.stringify(results, null, 2);
+  res.status(200).send(formattedResponse);
+});
+
+
+
+export default router;
